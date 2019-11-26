7410f77bf11ad25a0631df49a1ffb23784739c3c
henrykurg <kurg.henry@khk.ee>
2019-11-26 09:06:15 +0200

    vormi töötlus ja andmete käsitlus

---
+++ arrays/katsetamine.php
@@ -0,0 +1,41 @@
+
+<?php
+$nimed = array('mari', 'kati', 'juhan', 'miku', 'uku');
+
+//massiivi sisu koos parameetritega
+var_dump($nimed);
+//massiivi sisu kuvamine
+print_r($nimed);
+?>
+
+<br><br>
+
+<?php
+$nimed = array('mari', 'kati', 'juhan', 'miku', 'uku');
+echo $nimed[0];
+?>
+
+<br><br>
+
+<?php
+$nimed = array('mari', 'kati', 'juhan', 'miku', 'uku');
+
+//massiivi kõigi elementide väljastamine
+foreach($nimed as $nimi){
+    echo "$nimi <br>";
+}
+?>
+
+<br><br>
+
+<?php
+$hinded = array(
+    'kehv' => 2,
+    'rahuldav' => 3,
+    'hea' => 4,
+    'väga hea' => 5
+);
+foreach($hinded as $hinnang => $hinne){
+    echo "<li>Hinne: $hinne ($hinnang)</li>";
+}
+?>
---
+++ arrays/Ülesanne 5.php
@@ -0,0 +1,11 @@
+<?php
+$ained = array('eesti keel', 'matemaatika', 'kehaline', 'keemia', 'füüsika');
+
+
+sort($ained);
+
+foreach($ained as $aine){
+        echo "$aine <br>";
+    }
+?>
+
