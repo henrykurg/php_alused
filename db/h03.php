d3bd9a8b28c9f3b32404af729e2b414bf234614c
henrykurg <kurg.henry@khk.ee>
2020-01-12 21:39:19 +0200

    db - h03 - otsing andmebaasis

---
+++ db/h03.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'config.php'; // loeme andmebaasi conf sisse
+require_once 'db_fnc.php'; // loeme andmebaasi töötlusega seotud fun-onid
+require_once 'ui.php'; // loeme väljundfunktsioonid
+$ikt = connection(HOSTNAME, USERNAME, USERPASS, DBNAME);
+otsing();
+if(isset($_GET['otsi'])){
+    $otsi = $_GET['otsi'];
+    //lisame otsingusõna päringusse
+    $sql = 'SELECT 2015,Kool FROM koolid2015 WHERE Kool LIKE "%'.$otsi.'%"';
+    $result = getData($sql, $ikt);
+    table01($result, array('Kool', '2015'));
+}
